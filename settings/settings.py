a5e306acbf6e4e52aebc2f9f1b7d4e8236e4d32a
Lancine Sylla <mdslasso@gmail.com>
2023-12-13 18:25:31 +0100

    launching to production

--- settings/settings.py
+++ settings/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-s9*bazszr*y=^#3ha()d%t=u%or&+1v56hg%mml_8jabuss3ua
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
