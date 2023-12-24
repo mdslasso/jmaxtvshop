790d125f897cfaee8a5ad583d124669a8609570d
Lancine Sylla <mdslasso@gmail.com>
2023-12-24 12:31:37 +0100

    launching to production

--- settings/settings.py
+++ settings/settings.py
@@ -67,8 +67,13 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jmaxtvshop_db',
+        'USER': 'postgres',
+        'PASSWORD': 'bintou',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
 }
 
